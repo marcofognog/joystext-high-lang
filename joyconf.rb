480fece82d07824602d6f156ab0932f296b110bf
Marcao <marcofognog@gmail.com>
2018-01-21 15:41:36 -0200

    add support for remaps

--- joyconf.rb
+++ joyconf.rb
@@ -6,6 +6,7 @@ class Joyconf
     next_number = 0
     modes = {}
     mode_code = 0
+    remap_key = nil
 
     source.lines.each do |line|
       if line.split(' ').first == 'mode'
@@ -16,25 +17,28 @@ class Joyconf
     end
 
     source.lines.each do |line|
-      sanitized = line.split('#').first
+      sanitized = line.split('#').first.gsub("\n",'')
 
       if sanitized.split(' ').first == 'mode'
         current_mode = line.split(' ').last.gsub("'",'')
         mode_code = modes[current_mode]
-      elsif sanitized == "\n" || sanitized == ""
+      elsif sanitized.split(' ').first == 'remap'
+        remap_key = sanitized.split(' ')[1]
+      elsif sanitized == '}'.gsub(' ','')
+        inside_remap = nil
+      elsif sanitized == "".gsub(' ','')
       else
         splitted = sanitized.split(':')
         button_name = splitted[0]
         cmd = splitted[1].gsub("\n",'').gsub(' ','')
 
         if cmd =~ /switch_to_mode/
-          name_position = cmd =~ /\'.*?\'/
-          mode_name = cmd[(name_position + 1)..(cmd.length - 2)]
-          cmd = "switch_to_mode#{modes[mode_name]}"
+          cmd = build_switch_mode(cmd, modes)
         end
 
         trigger = trigger_code(button_name)
 
+        output << "#{remap_key}:=,#{mode_code}0" if remap_key
         output << "#{button_name[-2..-1]}:#{cmd},#{mode_code}#{trigger}"
       end
     end
@@ -43,6 +47,12 @@ class Joyconf
     result << "\n"
 
     return result
+  end
+
+  def self.build_switch_mode(cmd, modes)
+    name_position = cmd =~ /\'.*?\'/
+    mode_name = cmd[(name_position + 1)..(cmd.length - 2)]
+    return "switch_to_mode#{modes[mode_name]}"
   end
 
   def self.trigger_code(button_name)
