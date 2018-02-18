c1a1f8aefc67e670e3b4ef0593c05407d82187ab
Marcao <marcofognog@gmail.com>
2018-02-18 11:17:37 -0300

    These don't need to be in a helper anymore

--- lib/parse_helper.rb
+++ lib/parse_helper.rb
@@ -2,20 +2,4 @@ module ParseHelper
   def sanitized_button_name(name)
     name.delete('.').delete('<').delete('>').delete('*')
   end
-
-  def build_switch_mode(cmd, modes)
-    name_position = cmd =~ /\'.*?\'/
-    raise Joyconf::SwitchModeWithoutTarget,
-          'Syntax error: switch_mode needs a taget' if name_position.nil?
-    mode_name = cmd[(name_position + 1)..(cmd.length - 2)]
-    "switch_to_mode#{modes[mode_name]}"
-  end
-
-  def trigger_code(button_name)
-    return '1' if button_name =~ /\./
-    return '4' if button_name =~ /\</
-    return '3' if button_name =~ /\>/
-    return '2' if button_name =~ /\*/
-    return '0'
-  end
 end
